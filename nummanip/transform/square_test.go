bb6ae47ea55cbc2bd342a2f1d413cf31f5e267cd
Andreas Lind <Andreaslind31@gmail.com>
2021-08-01 14:04:20 +0200

    fixed failed test

--- nummanip/transform/square_test.go
+++ nummanip/transform/square_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 //test case for Square func in square.go
-func TestTransformSquare( t *testing.T)  {
-	testSlice := []int{1,2,3,4,5,6,7,8,9}
-	expectedResult := []int{1,4,9,16,25,36,49,64,81}
+func TestTransformSquare(t *testing.T) {
+	testSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	expectedResult := []int{1, 4, 9, 16, 25, 36, 49, 64, 81}
 
-	result := SquareSlice( testSlice)
+	result := SquareSlice(testSlice)
 
 	if reflect.DeepEqual(expectedResult, result) {
 		t.Log("SquareSlice PASSED")
